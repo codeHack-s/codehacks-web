ec6a4dd692bdbbbdde2e41927ae48f8d30df4747
Steve Tom <tomsteve187@gmail.com>
2023-09-21 12:34:02 +0300

    Fixed styles on select-account-type.blade.php

--- resources/views/auth/select-account-type.blade.php
+++ resources/views/auth/select-account-type.blade.php
@@ -1,14 +1,19 @@
 <x-guest-layout>
 
-<div class="flex justify-center flex-col gap-6 m-3">
+<div class="flex justify-center flex-col gap-6 my-3">
 
     <section class=" w-full flex text-3xl justify-center font-semibold items-center">
         <center>Select Account Type</center>
     </section>
 
-    <section class="links flex gap-3">
-        <a class="btn btn-outline btn-primary ring normal-case" href="{{ route('register', ['type' => 'campus']) }}">Codehacks Campus</a>
-        <a class="btn btn-outline btn-warning ring normal-case" href="{{ route('register', ['type' => 'innovate']) }}">Codehacks Innovate</a>
+    <section class="links flex mb-4 gap-3">
+        <div data-tip="For Chuka Uni students"  class="tooltip tooltip-bottom">
+            <a class="btn btn-outline btn-primary ring normal-case" href="{{ route('register', ['type' => 'campus']) }}">Codehacks Campus</a>
+        </div>
+        <div data-tip="Online Learning"  class="tooltip tooltip-bottom">
+            <a class="btn btn-outline btn-warning ring normal-case" href="{{ route('register', ['type' => 'innovate']) }}">Codehacks Innovate</a>
+        </div>
+
     </section>
 
 </div>
