ab0bc0554f2224ae5e5b581e5b271d9d50042168
Steve Tom <tomsteve187@gmail.com>
2023-09-21 12:25:16 +0300

    Added styles to select-account-type.blade.php

--- resources/views/auth/select-account-type.blade.php
+++ resources/views/auth/select-account-type.blade.php
@@ -1,6 +1,16 @@
-<!-- resources/views/select-account-type.blade.php -->
+<x-guest-layout>
 
-<h2>Select Account Type</h2>
+<div class="flex justify-center flex-col gap-6 m-3">
 
-<a href="{{ route('register', ['type' => 'campus']) }}">Codehacks Campus</a>
-<a href="{{ route('register', ['type' => 'innovate']) }}">Codehacks Innovate</a>
+    <section class=" w-full flex text-3xl justify-center font-semibold items-center">
+        <center>Select Account Type</center>
+    </section>
+
+    <section class="links flex gap-3">
+        <a class="btn btn-outline btn-primary ring normal-case" href="{{ route('register', ['type' => 'campus']) }}">Codehacks Campus</a>
+        <a class="btn btn-outline btn-warning ring normal-case" href="{{ route('register', ['type' => 'innovate']) }}">Codehacks Innovate</a>
+    </section>
+
+</div>
+
+</x-guest-layout>
