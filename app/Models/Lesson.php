5e5d70d37d5c5443f927ef6c1873a3cb17683a89
Steve Tom <tomsteve187@gmail.com>
2023-09-25 17:22:14 +0300

    --UI fixes and major Lesson view edit

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -23,6 +23,21 @@ class Lesson extends Model
         'date' => 'datetime',
     ];
 
+    public function getImageAttribute(): string
+    {
+        $randomImages = [
+            'https://img.freepik.com/free-vector/web-development-programmer-engineering-coding-website-augmented-reality-interface-screens-developer-project-engineer-programming-software-application-design-cartoon-illustration_107791-3863.jpg?w=996&t=st=1695650677~exp=1695651277~hmac=d554a74b40267beed9e2835c04aacbf61950baa711e034c4c3f6777ff94b7c8d',
+            'https://img.freepik.com/free-vector/programming-concept-illustration_114360-1351.jpg?w=740&t=st=1695650824~exp=1695651424~hmac=fd8c07236157a079cff705fe97f84bb49528bc4321483f72ca3635f4b7cf2f8a',
+            'https://img.freepik.com/free-photo/programming-background-with-person-working-with-codes-computer_23-2150010125.jpg?w=996&t=st=1695650859~exp=1695651459~hmac=1c34ef9f09f4689b00a44fe41e754fa7a8f4cee893c1a4cab8ed031dcbc38455',
+            'https://img.freepik.com/free-photo/programming-background-with-person-working-with-codes-computer_23-2150010142.jpg?w=996&t=st=1695650899~exp=1695651499~hmac=c4f2155ab349e02b32329f58a8de83cb8f70b1d11438f735bb01d8d0288e4cd5',
+            'https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg?w=826&t=st=1695650924~exp=1695651524~hmac=d9b7c839556fc344965ee14c4b0c284b5a5286d7584e2fae8782ee2ae89d62c3',
+            'https://img.freepik.com/free-vector/desktop-smartphone-app-development_23-2148683810.jpg?w=740&t=st=1695650956~exp=1695651556~hmac=4bdd10ac7358573205a39b938bfb4930840720f01d62ccc04a02efc4224f6a13',
+            'https://img.freepik.com/free-vector/programmer-concept-illustration_114360-2417.jpg?w=740&t=st=1695651006~exp=1695651606~hmac=d2515bb6457a81ce1f50701e2c2e32f22eb4382c97eb5c301a3896729189c87f'
+        ];
+
+        return $randomImages[array_rand($randomImages)];
+    }
+
     public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(User::class);
--- resources/views/lessons/show.blade.php
+++ resources/views/lessons/show.blade.php
@@ -2,57 +2,100 @@
 
     @include('session.alerts')
 
-    <section class="bg-gray-100  px-2 sm:px-6 py-2 sm:py-8">
-        <div class="max-w-4xl mx-auto md:flex">
-            <div class="md:flex-shrink-0 md:mr-6">
-                <!-- image -->
+    <section class="px-2 sm:px-6 py-2 sm:py-8">
+        <div class="max-w-4xl mx-auto md:grid md:grid-cols-2 gap-6">
+
+            <!-- Image Column -->
+            <div class="flex justify-center items-center md:order-1">
+                <div class="bg-gray-300 rounded-md flex justify-center items-center">
+                    <img src="{{ $lesson->image }}" alt="{{ $lesson->title }}" class="rounded-md">
+                </div>
             </div>
-            <div class="mt-4 flex flex-col gap-2 md:mt-0">
-                <h1 class="text-2xl md:text-3xl text-gray-700 font-semibold my-4 md:my-6">{{ $lesson->title }}</h1>
-                <p class="text-gray-600">{{ $lesson->description }}</p>
-                <p class="text-gray-600">Date: {{ $lesson->date }}</p>
-                <p class="text-gray-600">Venue: {{ $lesson->venue }}</p>
-                <p class="text-gray-600">Course: {{ $lesson->course->title }}</p>
-                <p>Attending: {{ $lesson->attending_members_count }}</p>
-                @if (!Auth::user()->lessons->contains($lesson->id))
-                    <form class="my-2 md:my-3" method="POST" action="{{ route('lesson.attend', $lesson) }}">
-                        @csrf
-                        @method('POST')
-                        <p class="my-2 md:my-3 text-red-500">You are not attending !</p>
-                        <input class="hidden" value="{{ Auth::user()->id }}" type="text" name="user_id">
-                        <x-primary-button class="ring-orange-400 w-[150px] rounded-full">
-                            Attend <i class="fa-solid fa-graduation-cap"></i>
-                        </x-primary-button>
-                    </form>
-                @else
-                    <form class="my-2 md:my-3" method="POST" action="{{ route('lesson.unattend', $lesson) }}">
-                        @csrf
-                        @method('POST')
-                        <input class="hidden" value="{{ Auth::user()->id }}" type="text" name="user_id">
+
+            <!-- Details Column -->
+            <div class="mt-4 md:mt-0 md:order-2">
+                <div class="border rounded-lg p-6 shadow-sm bg-white">
+                    <h1 class="text-2xl md:text-3xl text-gray-700 font-semibold mb-4">
+                        <i class="fas fa-heading text-gray-500 mr-2"></i> <!-- FontAwesome Icon -->
+                        {{ $lesson->title }}
+                    </h1>
+                    <p class="text-gray-600 mb-2">
+                        <i class="fas fa-info-circle text-gray-500 mr-2"></i> <!-- FontAwesome Icon -->
+                        {{ $lesson->description }}
+                    </p>
+                    <p class="text-gray-600 mb-2">
+                        <i class="fas fa-calendar-alt text-gray-500 mr-2"></i> <!-- FontAwesome Icon -->
+                        Date: {{ $lesson->date->format('F j, Y') }}
+                    </p>
+                    <p class="text-gray-600 mb-2">
+                        <i class="fas fa-map-marker-alt text-gray-500 mr-2"></i> <!-- FontAwesome Icon -->
+                        Venue: {{ $lesson->venue }}
+                    </p>
+                    <p class="text-gray-600 mb-2">
+                        <i class="fas fa-book text-gray-500 mr-2"></i> <!-- FontAwesome Icon -->
+                        Course: {{ $lesson->course->title }}
+                    </p>
+                    <p class="text-gray-600">
+                        <i class="fas fa-users text-gray-500 mr-2"></i> <!-- FontAwesome Icon -->
+                        Attending: {{ $lesson->attending_members_count }}
+                    </p>
+                </div>
+
+
+                <section class="flex items-center justify-center">
+                    @if (!Auth::user()->lessons->contains($lesson->id))
+                        <p class="my-2 md:my-3 text-red-500">You are not attending!</p>
+                    @else
                         <p class="my-2 md:my-3 text-green-500">You are attending the lesson</p>
-                        <x-primary-button class="ring-orange-400 rounded-full">
-                            Un-attend <i class="fa-solid fa-minus-circle"></i>
-                        </x-primary-button>
-                    </form>
-                @endif
+                    @endif
+                </section>
 
-                @can('manage')
-                    <div class="flex gap-3 items-center">
-                        <a href="{{ route('lessons.edit', $lesson) }}">
-                            <x-round-button class="btn hover:bg-blue-500">
-                                <i class="fa-solid fa-edit"></i>
-                            </x-round-button>
-                        </a>
-                        <form method="POST" action="{{ route('lessons.destroy', $lesson) }}">
-                            @csrf
-                            @method('DELETE')
-                            <x-round-button type="submit" class="btn hover:bg-red-500 btn-danger">
-                                <i class="fa-solid fa-trash"></i>
-                            </x-round-button>
-                        </form>
-                    </div>
+                <section class="flex flex-col md:flex-row justify-between mt-4 items-center">
 
-                @endcan
+                    @if (!Auth::user()->lessons->contains($lesson->id))
+                        <div class="mb-4 md:mb-0">
+                            <form class="my-2 md:my-3" method="POST" action="{{ route('lesson.attend', $lesson) }}">
+                                @csrf
+                                @method('POST')
+                                <input class="hidden" value="{{ Auth::user()->id }}" type="text" name="user_id">
+                                <x-primary-button class="ring-orange-400 w-[150px] rounded-full">
+                                    Attend <i class="fa-solid fa-graduation-cap"></i>
+                                </x-primary-button>
+                            </form>
+                        </div>
+                    @else
+                        <div class="mb-4 md:mb-0">
+                            <form class="my-2 md:my-3" method="POST" action="{{ route('lesson.unattend', $lesson) }}">
+                                @csrf
+                                @method('POST')
+                                <input class="hidden" value="{{ Auth::user()->id }}" type="text" name="user_id">
+                                <x-primary-button class="ring-orange-400 rounded-full">
+                                    Un-attend <i class="fa-solid fa-minus-circle"></i>
+                                </x-primary-button>
+                            </form>
+                        </div>
+                    @endif
+
+                    @can('manage')
+                        <div class="flex gap-3 items-center">
+                            <a href="{{ route('lessons.edit', $lesson) }}">
+                                <x-round-button class="btn hover:bg-blue-500">
+                                    <i class="fa-solid fa-edit"></i>
+                                </x-round-button>
+                            </a>
+                            <form method="POST" action="{{ route('lessons.destroy', $lesson) }}">
+                                @csrf
+                                @method('DELETE')
+                                <x-round-button type="submit" class="btn hover:bg-red-500 btn-danger">
+                                    <i class="fa-solid fa-trash"></i>
+                                </x-round-button>
+                            </form>
+                        </div>
+                    @endcan
+
+                </section>
+
+
             </div>
         </div>
     </section>
