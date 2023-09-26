4bb3534182c3d249819a4113f007305176eb3bd6
Steve Tom <tomsteve187@gmail.com>
2023-09-26 08:23:55 +0300

    Added favicon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,6 +7,10 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="{{ asset('storage/images/logo.png') }}" type="image/x-icon">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
