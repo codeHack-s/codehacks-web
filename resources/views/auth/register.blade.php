7e24c4d0e97142efaa8b3d62c0ee47500351cbbc
Raccoon254 <tomsteve187@gmail.com>
2024-01-02 16:19:17 +0300

    Register Page Cleanup

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,14 +39,8 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
+        <button class="w-full mt-4 btn btn-secondary ring-gray-400 ring-offset-1 ring-1 ring-inset btn-outline">
+            {{ __('Register') }}
+        </button>
     </form>
 </x-guest-layout>
